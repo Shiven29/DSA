fc75810e09b6b2de3ac86fab67dc40d2ffefe2ab
Shiven29 <99020850+Shiven29@users.noreply.github.com>
2026-08-25 00:48:44 +0530

    Time: 1 ms (99.99%) | Memory: 43.3 MB (77.88%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,19 +1,30 @@
 class Solution {
 
+    Boolean[][] memo;
+
     private boolean match(String s, String p , int i , int j){
+        if(memo[i][j] != null){
+            return memo[i][j];
+        }
         if(j == p.length()){
-            return i == s.length();
+            memo[i][j] = i == s.length();
+            return memo[i][j];
         }
         boolean firstMatch = i<s.length() && (s.charAt(i)==p.charAt(j)||p.charAt(j) == '.');
 
         if(j+1<p.length() && p.charAt(j+1) == '*'){
-            return match(s,p,i,j+2) || (firstMatch && match(s,p,i+1,j));
+            boolean answer = match(s,p,i,j+2) || (firstMatch && match(s,p,i+1,j));
+            memo[i][j] = answer;
+            return answer;
         }
 
-        return firstMatch && match(s,p,i+1,j+1);
+        boolean answer =  firstMatch && match(s,p,i+1,j+1);
+        memo[i][j] = answer;
+        return answer;
     }
 
     public boolean isMatch(String s, String p) {
+        memo = new Boolean[s.length()+1][p.length()+1];
         return match(s,p,0,0);
     }
 }
