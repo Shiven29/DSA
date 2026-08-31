c8d1954b8cac79c07eaa1386be7aa7018610e409
Shiven29 <99020850+Shiven29@users.noreply.github.com>
2026-08-31 19:21:56 +0530

    Time: 4 ms (79.65%) | Memory: 46.9 MB (46.49%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,38 +9,25 @@
  * }
  */
 class Solution {
-
-    private ListNode mergeTwoLists(ListNode list1 , ListNode list2){
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b)->a.val-b.val);
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
 
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){
-                current.next = list1;
-                list1 = list1.next;
-            }else{
-                current.next = list2;
-                list2 = list2.next;
+        for(int i=0; i<lists.length; i++){
+            if(lists[i]!=null){
+                pq.add(lists[i]);
             }
+        }
+        while(!pq.isEmpty()){
+            ListNode node = pq.poll();
+            current.next = node;
             current = current.next;
-        }
-        if(list1!=null){
-            current.next= list1;
-        }
-        if(list2!=null){
-            current.next = list2;
+
+            if(node.next!=null){
+                pq.add(node.next);
+            }
         }
         return dummy.next;
-    }
-
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0){
-            return null;
-        }
-        ListNode join = lists[0];
-        for(int i=1; i<lists.length; i++){
-            join = mergeTwoLists(join , lists[i]);
-        }
-        return join;
     }
 }
